493c07576dbc82483f7f04adc3ef7279f13a1a37
GUIGMA ALAIN <alainguigma1999@gmail.com>
2021-02-18 19:55:00 +0100

    suite

--- BD/plateforme_forum.sql
+++ BD/plateforme_forum.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le : ven. 12 fév. 2021 à 11:13
--- Version du serveur :  10.4.14-MariaDB
--- Version de PHP : 7.4.10
+-- Hôte : 127.0.0.1
+-- Généré le :  jeu. 18 fév. 2021 à 19:52
+-- Version du serveur :  10.3.15-MariaDB
+-- Version de PHP :  7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,16 +19,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `plateforme_forum`
+-- Base de données :  `plateforme_forum`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Inscription`
+-- Structure de la table `inscription`
 --
 
-CREATE TABLE `Inscription` (
+CREATE TABLE `inscription` (
   `ID` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -35,33 +36,99 @@ CREATE TABLE `Inscription` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `Inscription`
+-- Déchargement des données de la table `inscription`
 --
 
-INSERT INTO `Inscription` (`ID`, `username`, `email`, `password`) VALUES
-(1, 'hyjiu', 'nfhn@gmail.com', '$2y$10$AqBv/ZMfC91pFUkC1FdBX.bZJd20emX36TCaJ2djVramb1.FA./Nq'),
-(2, 'Alino', 'alainguigma99@gmail.com', '$2y$10$dN.wlWiMDT6HV5bnDhntfO5ddKdYCgO5H/JqJQ7I6gCzDfW6I.xUe'),
-(3, 'rebecca', 'rebe@gmail.com', '$2y$10$31pvaa7ZxP.AHTeykcDqM.lwgv5b7yoeOzYUGmUSN0NAWF4Xt9WrS');
+INSERT INTO `inscription` (`ID`, `username`, `email`, `password`) VALUES
+(4, 'Alain', 'alain@gmail.com', '$2y$10$jqCA/jGDUk87kdt9SaczDuWLqaklZ7AgPm.F48z7v0ZiLWgQrdfoW');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `poster`
+--
+
+CREATE TABLE `poster` (
+  `ID` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `categorie` varchar(50) NOT NULL,
+  `sous_categorie` varchar(100) NOT NULL,
+  `sujet` text NOT NULL,
+  `question` text NOT NULL,
+  `date_a` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `poster`
+--
+
+INSERT INTO `poster` (`ID`, `username`, `categorie`, `sous_categorie`, `sujet`, `question`, `date_a`) VALUES
+(3, 'oui', 'js', 'jq', 'c\'es koi', 'j me rapelle pas', '2021-02-18 12:08:04');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `poste_reponse`
+--
+
+CREATE TABLE `poste_reponse` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `reponses` text NOT NULL,
+  `date_a` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `poste_reponse`
+--
+
+INSERT INTO `poste_reponse` (`id`, `username`, `reponses`, `date_a`) VALUES
+(1, 'wiz', 'yeahh i am boss', '2021-02-18 19:16:22'),
+(2, 'oui', 'j\'ai oubliÃ©', '2021-02-18 19:16:22');
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `Inscription`
+-- Index pour la table `inscription`
 --
-ALTER TABLE `Inscription`
+ALTER TABLE `inscription`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Index pour la table `poster`
+--
+ALTER TABLE `poster`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Index pour la table `poste_reponse`
+--
+ALTER TABLE `poste_reponse`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT pour la table `Inscription`
+-- AUTO_INCREMENT pour la table `inscription`
 --
-ALTER TABLE `Inscription`
+ALTER TABLE `inscription`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `poster`
+--
+ALTER TABLE `poster`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `poste_reponse`
+--
+ALTER TABLE `poste_reponse`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
